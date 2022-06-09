faba901d17fe043dfb6bc54ff4f4a4e0218132a0
周必川 <zhoubichuan@icloud.com>
2022-06-09 19:46:01 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -24,20 +24,3 @@ git add -A
 git commit -m 'master'
 git pull github master
 git push github master
-
-# 2.打包
-npm run src:build
-
-# 3.进入打包目录
-cd dist
-
-# 4.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add github https://github.com/zhoubichuan/web-openlayers.git
-git push -f github gh-pages
-
-# 5.返回初始目录
-cd -
