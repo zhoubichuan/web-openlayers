7164425e01af6ef3c5615cdf9d1c93deeeca7d6d
zhoubichuan <zhoubichuan@icloud.com>
2022-06-11 00:02:15 +0800

    master

--- github.sh
+++ github.sh
@@ -1,18 +1,5 @@
-# 自动化打包同时将打包后的代码发布到gh-pages分支
-
-# 1.打包
-npm run src:build
-
-# 2.进入打包目录
-cd dist
-
-# 3.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add github https://github.com/zhoubichuan/web-openlayers.git
-git push -f github gh-pages
-
-# 4.返回初始目录
-cd -
+# # 1.发布到master分支
+git add -A
+git commit -m 'master'
+git pull github master
+git push github master
