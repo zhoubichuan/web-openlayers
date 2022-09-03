2cb2dd4ad1f55b5b902e1b03754d4f996b122604
zhoubichuan <zhoubichuan@icloud.com>
2022-09-04 00:39:09 +0800

    master

--- src/.vuepress/public/js/key.js
+++ src/.vuepress/public/js/key.js
@@ -2,4 +2,3 @@ var mapkeys = {
     maptiler: "pk.eyJ1IjoiemhvdWJpY2h1YW4iLCJhIjoiY2wyZGRleThpMHk5YjNkcDkzMWNveXB0YSJ9.L0LaQDinn6bNDLgQQmcuxQ",
     BingMaps:"At3uaebuh_acwoQWMvO4wxKUk91AzfgBEsx5kN37B7XNBL6dI5IQvtKr0fjZvYJc"
 }
-var _localStorage = localStorage
