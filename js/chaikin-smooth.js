0e2bfcfdecdde2917c1785652467c858f77ea573
周必川 <zhoubichuan@icloud.com>
2023-07-09 21:27:38 +0800

    gh-pages

---
+++ js/chaikin-smooth.js
@@ -0,0 +1,28 @@
+var copy = function vec2Copy(out, a) {
+    out[0] = a[0]
+    out[1] = a[1]
+    return out
+}
+function smooth(input, output) {
+    if (!Array.isArray(output))
+        output = []
+
+    if (input.length > 0)
+        output.push(copy([0, 0], input[0]))
+    for (var i = 0;i < input.length - 1;i++) {
+        var p0 = input[i]
+        var p1 = input[i + 1]
+        var p0x = p0[0],
+            p0y = p0[1],
+            p1x = p1[0],
+            p1y = p1[1]
+
+        var Q = [0.75 * p0x + 0.25 * p1x, 0.75 * p0y + 0.25 * p1y]
+        var R = [0.25 * p0x + 0.75 * p1x, 0.25 * p0y + 0.75 * p1y]
+        output.push(Q)
+        output.push(R)
+    }
+    if (input.length > 1)
+        output.push(copy([0, 0], input[input.length - 1]))
+    return output
+}
---
+++ js/key.js
@@ -0,0 +1,6 @@
+var mapkeys = {
+    mapbox: "pk.eyJ1IjoiemhvdWJpY2h1YW4iLCJhIjoiY2wyZGRleThpMHk5YjNkcDkzMWNveXB0YSJ9.L0LaQDinn6bNDLgQQmcuxQ",
+    maptiler: "get_your_own_D6rA4zTHduk6KOKTXzGB",
+    BingMaps: "At3uaebuh_acwoQWMvO4wxKUk91AzfgBEsx5kN37B7XNBL6dI5IQvtKr0fjZvYJc",
+    scalgo:"CC5BF28A7D96B320C7DFBFD1236B5BEB"
+}
